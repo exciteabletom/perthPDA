49053707f53b898a01aba6a87ae289f54295f737
exciteabletom <tom@digitalnook.net>
2022-03-31 20:03:25 +0800

    add config file

---
+++ config-example.py
@@ -0,0 +1,19 @@
+LICENSE_NUMBER = ""
+EXPIRY_DATE = ""
+FIRST_NAME = ""
+LAST_NAME = ""
+DATE_OF_BIRTH = ""
+
+# Remove the locations you don't need
+LOCATIONS = (
+    "Cannington",
+    "Joondalup",
+    "Kelmscott"
+    "Mandurah",
+    "Midland",
+    "Mirrabooka",
+    "Rockingham",
+    "Success",
+    "West Perth",
+)
+
--- driving_test.py
+++ driving_test.py
@@ -1,18 +1,26 @@
-import os
 import signal
 import sys
 from time import sleep
 
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
+try:
+    from config import LICENSE_NUMBER, EXPIRY_DATE, FIRST_NAME, LAST_NAME, DATE_OF_BIRTH, LOCATIONS
+except ImportError:
+    print(
+        "Please copy config-example.py to config.py and add your personal info :)",
+        file=sys.stderr,
+    )
+    sys.exit(1)
+
 options = Options()
-options.add_argument("--headless")
+#options.add_argument("--headless")
 
 driver = webdriver.Firefox(options=options)
 
@@ -29,37 +37,22 @@ wait = WebDriverWait(driver, 5)
 
 
 def main():
-    info = {}
-    for key in (
-        "LICENSE_NUMBER",
-        "EXPIRY_DATE",
-        "FIRST_NAME",
-        "LAST_NAME",
-        "DATE_OF_BIRTH",
-    ):
-        breakpoint()
-        info[key] = os.getenv(key)
-
     driver.get("https://online.transport.wa.gov.au/pdabooking/manage/?0")
 
     driver.find_element(By.NAME, "clientDetailsPanel:licenceNumber").send_keys(
-        info["LICENSE_NUMBER"]
+        LICENSE_NUMBER
     )
 
     driver.find_element(By.NAME, "clientDetailsPanel:licenceExpiryDate").send_keys(
-        info["EXPIRY_DATE"]
+        EXPIRY_DATE
     )
 
-    driver.find_element(By.NAME, "clientDetailsPanel:firstName").send_keys(
-        info["FIRST_NAME"]
-    )
+    driver.find_element(By.NAME, "clientDetailsPanel:firstName").send_keys(FIRST_NAME)
 
-    driver.find_element(By.NAME, "clientDetailsPanel:lastName").send_keys(
-        info["LAST_NAME"]
-    )
+    driver.find_element(By.NAME, "clientDetailsPanel:lastName").send_keys(LAST_NAME)
 
     driver.find_element(By.NAME, "clientDetailsPanel:dateOfBirth").send_keys(
-        info["DATE_OF_BIRTH"]
+        DATE_OF_BIRTH
     )
 
     wait.until(ec.element_to_be_clickable((By.ID, "id5"))).click()
@@ -80,13 +73,7 @@ def main():
             )
         ).click()
 
-    locations = (
-        "West Perth",
-        "Midland",
-        "Mandurah",
-    )
-
-    for location in locations:
+    for location in LOCATIONS:
         select = Select(
             wait.until(
                 ec.element_to_be_clickable((By.NAME, "searchBookingContainer:siteCode"))
